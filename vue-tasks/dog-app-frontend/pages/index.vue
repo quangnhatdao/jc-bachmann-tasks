6735e35734290e378f61a7d6674d83b7b071283a
quangnhatdao <quangnhat3101@gmail.com>
2022-07-05 15:01:18 +0200

    implement button

---
+++ vue-tasks/dog-app-frontend/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <DogCardGroup v-if="showDogs"></DogCardGroup>
+    <div v-if="!showDogs" class="d-flex justify-content-center align-items-center vw-100 vh-100">
+      <b-button variant="primary" @click="showDogs = true">Dogs!</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import DogCardGroup from "~/components/DogCardGroup.vue";
+export default {
+  components: { DogCardGroup },
+  data() {
+    return {
+      showDogs: false,
+    }
+  },
+};
+</script>
