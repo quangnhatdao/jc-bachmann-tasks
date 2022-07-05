d22646051cb559549a1f84a30eede56b05b76f38
quangnhatdao <quangnhat3101@gmail.com>
2022-07-05 15:00:52 +0200

    implement dog card

---
+++ vue-tasks/dog-app-frontend/components/DogCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-card
+      :title="title"
+      :img-src="imgSrc"
+      :img-alt="imgAlt"
+      img-top
+      tag="article"
+      class="dog-card mb-2"
+    >
+      <b-card-text>
+        {{text}}
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+  </div>
+</template>
+
+<script>
+const axios = require("axios").default;
+
+export default {
+  data() {
+    return {
+      imgSrc: "https://via.placeholder.com/300",
+    }
+  },
+  props: {
+    title: String,
+    imgAlt: String,
+    text: String
+  },
+  mounted() {
+    var vm = this;
+
+    axios
+      .get(
+        "https://dog.ceo/api/breeds/image/random"
+      )
+      .then(function (response) {
+        console.log(response.data);
+        vm.imgSrc = response.data.message;
+      })
+      .then(function () {});
+  },
+}
+</script>
+
+<style scoped>
+.dog-card {
+}
+</style>
