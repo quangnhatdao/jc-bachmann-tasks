14c3dd6ba4ca2ea8c982cc795137d6f03d33eb74
quangnhatdao <quangnhat3101@gmail.com>
2022-07-05 15:01:04 +0200

    implement dog card columns

---
+++ vue-tasks/dog-app-frontend/components/DogCardGroup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-card-group columns>
+      <DogCard v-for="index in 20" :key="index" :title="'Dog # ' + index" imgAlt="Dog" text="Dog"></DogCard>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+import DogCard from "./DogCard.vue";
+
+export default {
+    components: { DogCard }
+}
+</script>
